1ef66f0bc67160b9ce7dd43fd4177e7b45139dc4
Razi <106505820+Razi-Azam@users.noreply.github.com>
2022-06-03 16:22:25 +0530

    Footer Added

--- js/script.js
+++ js/script.js
@@ -1,35 +1,30 @@
 const mobileNavButton = document.querySelector(".mobile-navbar-btn");
-
 const leftMenu = document.querySelector(".left-menu");
-
 const subMenu = document.querySelector(".sub-menu");
-
 const mainMenu = document.querySelector(".main-menu");
-
 const body = document.body;
 
-//function to add the 
+//function to add the
 function removeChildNode() {
-    const logo = document.querySelector('.logo');
-    logo.parentNode.insertBefore(leftMenu, logo);
+  const logo = document.querySelector(".logo");
+  logo.parentNode.insertBefore(leftMenu, logo);
 }
-
 
 //toggling the menu button in mobile mode
 const toggleNavBar = () => {
-    mainMenu.classList.toggle("active");
-    subMenu.classList.toggle("active");
-    subMenu.append(leftMenu);
-    leftMenu.classList.toggle("active");
+  mainMenu.classList.toggle("active");
+  subMenu.classList.toggle("active");
+  subMenu.append(leftMenu);
+  leftMenu.classList.toggle("active");
 
-    if(!(subMenu.classList.contains('active'))) {
-        removeChildNode();
-    }
+  if (!subMenu.classList.contains("active")) {
+    removeChildNode();
+  }
 
-    leftMenu.childNodes[1].style.paddingBottom = "30px";
-    leftMenu.childNodes[1].style.marginLeft = "-10px";
-    leftMenu.childNodes[3].style.marginLeft = "-10px";
-}
+  leftMenu.childNodes[1].style.paddingBottom = "30px";
+  leftMenu.childNodes[1].style.marginLeft = "-10px";
+  leftMenu.childNodes[3].style.marginLeft = "-10px";
+};
 
 //callig the toggleNavBar function on click event
-mobileNavButton.addEventListener("click", () => toggleNavBar());    
+mobileNavButton.addEventListener("click", () => toggleNavBar());
